a41b001446f3fd3fa188ebf0f68a307399faeba4
Rohitr234 <86827293+Rohitr234@users.noreply.github.com>
2021-07-03 12:47:39 +0530

    Add files via upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,91 @@
+@extends('layout')
+
+@section('main-content')
+    <div>
+        <div class="float-start">
+            <h4 class="pb-3">My Tasks</h4>
+        </div>
+        <div class="float-end">
+            <a href="{{ route('task.create') }}" class="btn btn-info">
+               <i class="fa fa-plus-circle"></i> Create Task
+            </a>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+
+    @foreach ($tasks as $task)
+        <div class="card mt-3">
+            <h5 class="card-header">
+                
+                    {{ $task->title }}
+               
+
+                <span class="badge rounded-pill bg-light text-dark">
+                    {{ $task->created_at->diffForHumans() }}
+                </span>
+            </h5>
+
+            <div class="card-body">
+                <div class="card-text">
+                    <div class="float-start">
+                        @if ($task->status === 'High')
+                            {{ $task->description }}
+                            @elseif ($task->status === 'Normal')
+                            {{ $task->description }}
+                            @elseif ($task->status === 'Low')
+                            {{ $task->description }}
+                        @else
+                            <del>{{ $task->description }}</del>
+                        @endif
+                        <br>
+
+                        @if ($task->status === 'High')
+                            <span class="badge rounded-pill bg-danger text-white">
+                                High
+                            </span>
+                        @elseif ($task->status === 'Normal')
+                        <span class="badge rounded-pill bg-info text-dark">
+                                Normal
+                            </span>
+                        @else
+                            <span class="badge rounded-pill bg-success text-white">
+                                Low
+                            </span>
+                        @endif
+
+
+                        <small>Last Updated - {{ $task->updated_at->diffForHumans() }} </small>
+                    </div>
+                    <div class="float-end">
+                        <a href="{{ route('task.edit', $task->id) }}" class="btn btn-success">
+                           <i class="fa fa-edit"></i>
+                        </a>
+
+                        <form action="{{ route('task.destroy', $task->id) }}" style="display: inline" method="POST" onsubmit="return confirm('Are you sure to delete ?')">
+                            @csrf
+                            @method('DELETE')
+
+                            <button type="submit" class="btn btn-danger">
+                                <i class="fa fa-trash"></i>
+                            </button>
+                        </form>
+
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+
+    @if (count($tasks) === 0)
+        <div class="alert alert-danger p-2">
+            No Task Found. Please Create one task
+            <br>
+            <br>
+            <a href="{{ route('task.create') }}" class="btn btn-info">
+                <i class="fa fa-plus-circle"></i> Create Task
+             </a>
+        </div>
+    @endif
+
+@endsection
